3fd71225f44f2649485ddd04284dda49911a3643
lestorrr <lesterliclican22@gmail.com>
2026-02-25 23:01:24 +0800

    fix: remove obsolete database files and update assembly version information

--- src/LibraryManagement.API/obj/Debug/net8.0/LibraryManagement.API.AssemblyInfo.cs
+++ src/LibraryManagement.API/obj/Debug/net8.0/LibraryManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa24b4ebe38b699e101bb1618c754ba1e4d6b99b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c324d93ed5045c3db038a1b03ba909d34ab227d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
